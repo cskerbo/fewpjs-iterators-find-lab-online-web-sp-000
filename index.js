23e280599207087df4c4eb43c243831ed5830b7a
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-24 21:51:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superBowlWin(array) {
+  if (array.find( data => data.result === "W")) {
+    return data.year
+  }
+  else {
+    return
+  }
+}
